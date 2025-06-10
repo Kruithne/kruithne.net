f049481aae9b3052dd92de9dc320c77d220c557b
Kruithne <kruithne@gmail.com>
2025-06-10 19:56:11 +0100

    implement basic site structure

---
+++ server.ts
@@ -0,0 +1,76 @@
+import { serve, caution, HTTP_STATUS_CODE, validate_req_json } from 'spooder';
+import path from 'node:path';
+import crypto from 'node:crypto';
+import { ColorInput } from 'bun';
+
+const server = serve(Number(process.env.SERVER_PORT), process.env.SERVER_LISTEN_HOST);
+
+const ANSI_RESET = '\x1b[0m';
+function log(message: string, color: ColorInput = 'purple'): void {
+	const ansi = Bun.color(color, 'ansi');
+	process.stdout.write(`[{info}] > ${message}\n`.replace(/\{([^}]+)\}/g, `${ansi}$1${ANSI_RESET}`));
+}
+
+// let index: string|null = null;
+// let index_hash: string|null = null;
+
+// server.route('/', async (req, _url) => {
+// 	if (index === null) {
+// 		index = await Bun.file('./html/index.html').text();
+// 		index_hash = crypto.createHash('sha256').update(index).digest('hex');
+// 	}
+	
+// 	const headers = {
+// 		'Content-Type': 'text/html',
+// 		'Access-Control-Allow-Origin':  '*',
+// 		'ETag': index_hash as string
+// 	} as Record<string, string>;
+	
+// 	if (req.headers.get('If-None-Match') === index_hash)
+// 		return new Response(null, { status: 304, headers }); // Not Modified
+	
+// 	return new Response(index, { status: 200, headers });
+// });
+
+server.route('/', () => {
+	const file = Bun.file('./html/index.html');
+	return new Response(file, { status: 200 });
+});
+
+server.dir('/wow.export', './wow.export', async (file_path, file, stat, _request) => {
+	// ignore hidden files
+	if (path.basename(file_path).startsWith('.'))
+		return 404; // Not Found
+	
+	// ignore directories
+	if (stat.isDirectory())
+		return 401; // Unauthorized
+	
+	return file;
+});
+
+async function default_handler(status_code: number): Promise<Response> {
+	return new Response(HTTP_STATUS_CODE[status_code], { status: status_code });
+}
+
+// Unhandled exceptions and rejections from handlers.
+server.error((err: Error) => {
+	caution(err?.message ?? err);
+	return default_handler(500);
+});
+
+// Unhandled response codes.
+server.default((_req, status_code) => default_handler(status_code));
+
+// Automatic update webhook
+if (typeof process.env.GH_WEBHOOK_SECRET === 'string') {
+	server.webhook(process.env.GH_WEBHOOK_SECRET, '/internal/hook_source_change', () => {
+		setImmediate(async () => {
+			await server.stop(false);
+			process.exit(0);
+		});
+		return 200;
+	});
+} else {
+	caution('GH_WEBHOOK_SECRET environment variable not configured');
+}
