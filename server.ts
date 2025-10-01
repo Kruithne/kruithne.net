dad6eb5a9bbcf8caa1230552317bb199af833371
Kruithne <kruithne@gmail.com>
2025-10-01 14:40:21 +0100

    add placeholder page

--- server.ts
+++ server.ts
@@ -69,7 +69,8 @@ server.default((req, status_code) => cache.request(
 server.route('/', (req, url) => {
 	return cache.request(req, '/', async () => {
 		return await parse_template(
-			await resolve_bootstrap_content(Bun.file('./html/index.html')),
+			//await resolve_bootstrap_content(Bun.file('./html/index.html')),
+			await resolve_bootstrap_content(Bun.file('./html/soon.html')),
 			global_sub_table,
 			false
 		);
