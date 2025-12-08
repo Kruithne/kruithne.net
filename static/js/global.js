d23c188d5a986d92a7c25e2a123d40b97899a633
Kruithne <kruithne@gmail.com>
2025-12-08 19:16:04 +0000

    improve hamburger menu functionality

--- static/js/global.js
+++ static/js/global.js
@@ -1,4 +1,26 @@
-document.getElementById('nav-toggle').addEventListener('click', function() {
-	document.getElementById('main-nav').classList.toggle('open');
-	document.getElementById('nav-links').classList.toggle('open');
+const navToggle = document.getElementById('nav-toggle');
+const mainNav = document.getElementById('main-nav');
+const navLinks = document.getElementById('nav-links');
+const navOverlay = document.getElementById('nav-overlay');
+
+function openNav() {
+	mainNav.classList.add('open');
+	navLinks.classList.add('open');
+	navOverlay.classList.add('open');
+}
+
+function closeNav() {
+	mainNav.classList.remove('open');
+	navLinks.classList.remove('open');
+	navOverlay.classList.remove('open');
+}
+
+navToggle.addEventListener('click', function(e) {
+	e.stopPropagation();
+	if (navLinks.classList.contains('open'))
+		closeNav();
+	else
+		openNav();
 });
+
+navOverlay.addEventListener('click', closeNav);
