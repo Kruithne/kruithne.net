7bedd16a43a9e79c3b7f618766ca31646e8fbcf6
Kruithne <kruithne@gmail.com>
2025-12-08 19:02:08 +0000

    add mobile hamburger navigation

---
+++ static/js/global.js
@@ -0,0 +1,4 @@
+document.getElementById('nav-toggle').addEventListener('click', function() {
+	document.getElementById('main-nav').classList.toggle('open');
+	document.getElementById('nav-links').classList.toggle('open');
+});
