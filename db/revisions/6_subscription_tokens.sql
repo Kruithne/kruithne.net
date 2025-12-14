83235c3a65b1a297a31e1f49d6d1c3d2d3cb65f1
Kruithne <kruithne@gmail.com>
2025-12-14 14:29:25 +0000

    fix sql for subscription tokens

--- db/revisions/6_subscription_tokens.sql
+++ db/revisions/6_subscription_tokens.sql
@@ -1,8 +1,8 @@
 -- Add unique token to post_subscriptions for VERP bounce tracking and secure unsubscribe
 ALTER TABLE post_subscriptions ADD COLUMN token VARCHAR(8) UNIQUE;
 
--- Generate tokens for existing subscriptions
-UPDATE post_subscriptions SET token = LOWER(SUBSTR(MD5(RANDOM()::text), 1, 8)) WHERE token IS NULL;
+-- Generate tokens for existing subscriptions (MySQL syntax)
+UPDATE post_subscriptions SET token = LOWER(SUBSTR(MD5(RAND()), 1, 8)) WHERE token IS NULL;
 
--- Make token required for new subscriptions
-ALTER TABLE post_subscriptions ALTER COLUMN token SET NOT NULL;
+-- Make token required for new subscriptions (MySQL syntax)
+ALTER TABLE post_subscriptions MODIFY COLUMN token VARCHAR(8) NOT NULL UNIQUE;
